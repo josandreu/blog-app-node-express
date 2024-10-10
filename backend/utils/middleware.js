48d909bd379a8276455885c18d97218de4d96af5
joseantonio_andreu <joseantonio.andreu@flat101.es>
2024-10-10 12:33:39 +0200

    update eslint

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -67,6 +67,10 @@ const errorHandler = (error, request, response, next) => {
       .json({ error: 'expected `username` to be unique' });
   }
 
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
   if (error.name === 'TokenExpiredError') {
     return response.status(400).json({ error: error.message });
   }
